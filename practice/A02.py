455bdb07f565fdc039b8602c341aecc5ed1961f9
1F10230048 <s1f102300481@iniad.org>
2024-08-30 15:37:29 +0900

    1-2

---
+++ practice/A02.py
@@ -0,0 +1,10 @@
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+answer = False
+for i in range(N):
+    if A[i] == X:
+        answer = True
+if answer == True:
+    print("Yes")
+else:
+    print("No")
